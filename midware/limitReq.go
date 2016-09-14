9645d59b829e1332a627f220d43858c7dea875fd
tangshiyou <tang.rosechild@gmail.com>
2016-09-14 17:41:08 +0800

    modify limit

--- midware/limitReq.go
+++ midware/limitReq.go
@@ -11,11 +11,26 @@ type HandlerLimit struct {
 	LimitIpMap map[string]chan struct{}
 }
 
+// LimitHandler default max req/ip is 10
 func LimitHandler(limit *HandlerLimit, handler http.HandlerFunc) http.HandlerFunc {
-
-	if limit.Mutex == nil {
+	switch {
+	case limit == nil:
+		limit = &HandlerLimit{
+			Mutex:      &sync.RWMutex{},
+			Max:        10,
+			LimitIpMap: make(map[string]chan struct{}),
+		}
+	case limit.Mutex == nil:
 		limit.Mutex = &sync.RWMutex{}
+		fallthrough
+	case limit.Max == 0:
+		limit.Max = 10
+		fallthrough
+	case limit.LimitIpMap == nil:
+		limit.LimitIpMap = make(map[string]chan struct{})
+
 	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		if _, ok := limit.LimitIpMap[r.Host]; !ok {
